bcda562cb824653fdad233f179956b1cf4db17d7
taruntata <141921014+taruntata@users.noreply.github.com>
2026-01-02 22:03:41 +0530

    Time: 669 ms (5.89%) | Memory: 18.6 MB (79.92%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,6 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                nums.remove(nums[i])
+                nums.append(0)

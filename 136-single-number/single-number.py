8aad5bce3f72220ef7ea57100d2862afb13cf53b
taruntata <141921014+taruntata@users.noreply.github.com>
2026-01-05 22:00:48 +0530

    Time: 1 ms (73.44%) | Memory: 19.2 MB (81.77%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        count = 0
+        sum = 0
         for num in nums:
-            if nums.count(num) != 2:
-                return num
+            sum ^= num
+        return sum
